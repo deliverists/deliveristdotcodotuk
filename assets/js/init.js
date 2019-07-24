e6c6414cb991637af1a0f8813255b86561ad79c0
Nick Meldrum <nick@nickmeldrum.com>
2019-07-24 07:38:12 +0100

    wip - cleaning up james template - horrified by the divitis and poor strucuture - marking this as a waypoint before starting from scratch with muicss instead

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,22 +1,20 @@
- (function($){
-   $(function(){
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav()
+    $('.parallax').parallax()
+    $('.collapsible').collapsible()
+    $('.carousel.carousel-slider').carousel({fullWidth: true})
+    $('.materialboxed').materialbox()
+    $('.scrollspy').scrollSpy()
+    $('.tap-target').tapTarget('open')
 
-     $('.button-collapse').sideNav();
-     $('.parallax').parallax();
-     $('.collapsible').collapsible();
-     $('.carousel.carousel-slider').carousel({fullWidth: true});
-     $('.materialboxed').materialbox();
-     $('.scrollspy').scrollSpy();
-     $('.tap-target').tapTarget('open');
+    if (localStorage.getItem('cookieconsent') !== 'true') {
+      $('#cookies').show()
+    }
 
-     if (localStorage.getItem('cookieconsent') === 'true') {
-       $('#cookies').hide()
-     }
-
-     jQuery('#cookies').on('click', function(event) {
-            localStorage.setItem('cookieconsent', 'true')
-            jQuery('#cookies').toggle('hide');
-       });
-
-   }); // end of document ready
- })(jQuery);
+    jQuery('#cookies').on('click', function(event) {
+      localStorage.setItem('cookieconsent', 'true')
+      jQuery('#cookies').toggle('hide')
+    })
+  })
+})(jQuery)
